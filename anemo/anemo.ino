ab36fcc2bd6faf2afaa4fb0dd615506d06a2394b
Juan J. Rojas <juanjorojash@gmail.com>
2020-10-18 22:15:32 -0600

    speed and averaging working now, I'm going for the serial menu

--- anemo/anemo.ino
+++ anemo/anemo.ino
@@ -1,16 +1,14 @@
 /**
- * @file pump.ino
- * @author Alejandro Arroyo
- * @author Sergio Solorzano
+ * @file anemo.ino
  * @author Juan J. Rojas 
  * @date 02 Oct 2020
- * @brief Main loop for pump controller
+ * @brief Main loop for LIENE anemometer
  * @par Institution:
- * SETECLab. Instituto Tecnologico de Costa Rica.
+ * DeltaLab. Instituto Tecnologico de Costa Rica.
  * @par mail:
  * juan.rojas@tec.ac.cr
- * @par Git repository:
- * https://github.com/Setec-Lab/gst_eps
+ * @par Github repository:
+ * https://github.com/DeltaLabo/anemos
  */
 
 // JR: VERY Important ADC2 pins cannot be used when the WiFi is being used, and some of the ADC1 pins are also blocked for some reason, 32 is ok...
@@ -26,41 +24,36 @@
 #define AIO_SERVERPORT  1883                  
 #define AIO_USERNAME    "JUANJOROJASH"
 #define AIO_KEY         "aio_WrBp37mZc3UAxXUr73RNh7SB9LQf"
+#define PUB_INTERVAL    2
 
-uint32_t reel_rotations = 0;
-bool reel_flag;
+uint32_t reed_rotations = 0;
+bool reed_flag;
 uint64_t current;
-uint64_t previous;
+uint64_t previous = rtc_time_get();
 uint64_t cycles_passed;
-double ms_passed = 0;
-double prev_ms_passed = 0;
+double curr_interval = 0;
+double prev_interval = 0;
 double avg_time_ms = 0;
-double curr_speed;
-double prev_speed;
+double anemo_speed = 0;
+double pub_speed = 0;
+double prev_speed = 0;
 uint32_t period_in_us = rtc_clk_cal(RTC_CAL_RTC_MUX,10);  //ten cycle average
 /* create a hardware timer */
 hw_timer_t * timer = NULL;
 bool timer_flag;
 int counter = 0;
-bool pub_flag = 0;
-
+bool pub_flag = 1;
+bool print_flag = 1;
+int pub_secs = 0; // in seconds
+int result = 0;
+char receive_buffer[20] = {'\0'};
 
 WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);       
 Adafruit_MQTT_Publish anemo_data = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/anemo_test");
 
-void IRAM_ATTR reel_isr() {
-  if(counter == 0){
-    previous = rtc_time_get();
-    ms_passed = 0;
-  }else{
-    current = rtc_time_get();
-    cycles_passed = current - previous;
-    previous = current;
-    prev_ms_passed = ms_passed;  
-    ms_passed += (double) ((rtc_time_slowclk_to_us(cycles_passed,period_in_us))/1000.0);
-  }
-  counter++;
+void IRAM_ATTR reed_isr() {
+  reed_flag = 1;
 }
 
 void IRAM_ATTR timer_isr(){
@@ -70,7 +63,7 @@ void IRAM_ATTR timer_isr(){
 void setup() {
   Serial.begin(115200);
   delay(10);
-  Serial.println(); Serial.println();
+  Serial.print('\n')
   Serial.print("Connecting to ");
   Serial.println(WLAN_SSID);
   WiFi.begin(WLAN_SSID, WLAN_PASS);
@@ -82,7 +75,7 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: "); Serial.println(WiFi.localIP());
   pinMode(2, INPUT);
-  attachInterrupt(2, &reel_isr, RISING);
+  attachInterrupt(2, &reed_isr, RISING);
   Serial.println("Start GPIO interrupt on PIN#2...");  
     /* Use 1st timer of 4 */
   /* 1 tick take 1/(80MHZ/80) = 1us so we set divider 80 and count up */
@@ -98,34 +91,62 @@ void setup() {
   Serial.println("Start timer..");
 }
 
-
+char telemetry[9] = {'\0'};
 
 void loop() {
-  MQTT_connect(); 
+  if(reed_flag){
+    reed_flag = 0;  
+    counter++;  
+    current = rtc_time_get(); //get current RTC time cycles
+    cycles_passed = current - previous; //calculate how many cycles have passed since previous reed switch closing   
+    prev_interval = curr_interval; //save previous interval in case the 1sec interruption ocurrs
+    curr_interval += (double) ((rtc_time_slowclk_to_us(cycles_passed,period_in_us))/1000.0);  //calculate current interval based on cycle period of rtc     
+    previous = current; //save previous cycles for next interruption    
+  }  
   if (timer_flag){  //if one second has passed
-    timer_flag = 0; //clear the flag 
-    Serial.print("ms_passed: ");
-    Serial.println(ms_passed);
-    Serial.print("counter: ");
-    Serial.println(counter);
-      if (counter < 2){
-        curr_speed = 0;   
+    timer_flag = 0;
+      if (counter == 0){
+        anemo_speed = 0;
       }else{
-        avg_time_ms = ms_passed / counter;
-        curr_speed = (1000/avg_time_ms)*2.5*0.44704;  
-        ms_passed = ms_passed - prev_ms_passed; //keep only last interval  
-        counter = 1;      
+        if (counter == 1 & curr_interval > 2500){ //below 2500cycles, ie below 0.45m/s report zero
+          curr_interval = 2500; 
+          anemo_speed = 0;       
+        }else{
+          avg_time_ms = curr_interval / counter;  //take the average of the overall interval between the rotations
+          anemo_speed = (1000/avg_time_ms)*2.5*0.44704;  //calculate speed according to the manufacturer
+          curr_interval = curr_interval - prev_interval;  //set the interval to the time between last rotation and the one second trigger         
+        }        
+        counter = 0;  //clear the counter to restart the speed calculation    
       }
-    Serial.print("Speed: ");
-    Serial.println(curr_speed);
-    if((curr_speed != prev_speed)&(pub_flag)){
-      if (! anemo_data.publish(curr_speed)) {
-        Serial.println(F("Failed"));
-      }else{
-        Serial.println(F("OK!"));
+    result = sprintf(telemetry, "S%05.2fN", anemo_speed); //create telemetry string before publising in MTTQ 
+    /*the string has the S as firsts character, then 5 spaces for the velocity including two before the dot and two after the dot, then
+    * the status of the MTTQ transmission 
+    */
+    
+    
+    if(pub_flag){
+      pub_secs++;
+      pub_speed += anemo_speed;
+      if (pub_secs == PUB_INTERVAL){ //only publish according to the preset interval
+        pub_speed /= pub_secs;
+        pub_secs = 0;       
+        MQTT_connect();
+        if(pub_speed != prev_speed){ //only publish if the value is different from the previous
+          if (! anemo_data.publish(pub_speed)) {
+            telemetry[6] = 'E';
+          }else{
+            telemetry[6] = 'S';
+          }
+          prev_speed = pub_speed;
+          Serial.print("pub_speed: ");
+          Serial.println(pub_speed);
+          pub_speed = 0;
+        }
       }
     }
-    prev_speed = curr_speed;    
+    telemetry[7] = '\n';
+    if (print_flag) Serial.print(telemetry); 
+
   }
 }
 
@@ -147,4 +168,9 @@ void MQTT_connect(){
     }    
   }
   Serial.println("MQTT Connected!");
+}
+
+void menu(){
+  Serial.print("Ingrese el SSID o presione enter para usar ");
+  Serial.println(WLAN_SSID);
 }
